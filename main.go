93223619ce2f3d08b84de26aeb8e473a39923937
Michael Lesniak <mail@mlesniak.com>
2020-01-15 14:13:41 +0100

    Remove obsolete function

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -29,27 +28,4 @@ func main() {
 	file, err := os.Create("demo.png")
 	must(err)
 	must(png.Encode(file, img))
-}
-
-// go run main.go|pnmtopng >demo.png && open demo.png
-func writePPM(nx int, ny int) {
-	// Header
-	fmt.Println("P3")
-	fmt.Println(nx, ny)
-	fmt.Println(255)
-
-	// Pixel
-	for j := ny - 1; j >= 0; j-- {
-		for i := 0; i < nx; i++ {
-			r := float64(i) / float64(nx)
-			g := float64(j) / float64(ny)
-			b := 0.2
-
-			ir := int(255.99 * r)
-			ig := int(255.99 * g)
-			ib := int(255.99 * b)
-
-			fmt.Println(ir, ig, ib)
-		}
-	}
 }
