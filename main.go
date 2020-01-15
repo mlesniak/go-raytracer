1404adb89f3bacb1d43056ad2f63b28df7bb67a1
Michael Lesniak <mail@mlesniak.com>
2020-01-15 15:42:38 +0100

    Add background computation with rays

--- main.go
+++ main.go
@@ -11,18 +11,22 @@ func main() {
 	nx := 200
 	ny := 100
 
+	origin := Vector{0.0, 0.0, 0.0}
+	lowerLeft := Vector{-2.0, -2.0, -1.0}
+	horizontal := Vector{4.0, 0.0, 0.0}
+	vertical := Vector{0.0, 2.0, 0.0}
+
 	img := image.NewRGBA(image.Rect(0, 0, nx, ny))
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			vec := Vector{
-				float64(i) / float64(nx),
-				float64(j) / float64(ny),
-				0.2,
-			}
+			u := float64(i) / float64(nx)
+			v := float64(j) / float64(ny)
+			r := Ray{origin, lowerLeft.Add(horizontal.Scale(u).Add(vertical.Scale(v)))}
 
-			ir := uint8(255.99 * vec.R())
-			ig := uint8(255.99 * vec.G())
-			ib := uint8(255.99 * vec.B())
+			c := backgroundColor(r)
+			ir := uint8(255.99 * c.R())
+			ig := uint8(255.99 * c.G())
+			ib := uint8(255.99 * c.B())
 
 			img.Set(i, ny-j, color.RGBA{ir, ig, ib, 255})
 		}
@@ -30,4 +34,12 @@ func main() {
 	file, err := os.Create("demo.png")
 	must(err)
 	must(png.Encode(file, img))
+}
+
+func backgroundColor(r Ray) Vector {
+	unitDirection := Unit(r.Direction())
+	t := 0.5 * (unitDirection.Y() + 1.0)
+	v1 := Vector{1.0, 1.0, 1.0}.Scale(1.0 - t)
+	v2 := Vector{0.5, 0.7, 1.0}.Scale(t)
+	return v1.Add(v2)
 }
