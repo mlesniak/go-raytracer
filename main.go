ebdb0f04b53ef8cbbe141dce100a8c0da8ecd8d5
Michael Lesniak <mail@mlesniak.com>
2020-01-15 14:13:16 +0100

    Write PNG file

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
 
-// go run main.go|pnmtopng >demo.png && open demo.png
 func main() {
 	nx := 200
 	ny := 100
 
+	img := image.NewRGBA(image.Rect(0, 0, nx, ny))
+	for j := ny - 1; j >= 0; j-- {
+		for i := 0; i < nx; i++ {
+			r := float64(i) / float64(nx)
+			g := float64(j) / float64(ny)
+			b := 0.2
+
+			ir := uint8(255.99 * r)
+			ig := uint8(255.99 * g)
+			ib := uint8(255.99 * b)
+
+			img.Set(i, ny-j, color.RGBA{ir, ig, ib, 255})
+		}
+	}
+	file, err := os.Create("demo.png")
+	must(err)
+	must(png.Encode(file, img))
+}
+
+// go run main.go|pnmtopng >demo.png && open demo.png
+func writePPM(nx int, ny int) {
 	// Header
 	fmt.Println("P3")
 	fmt.Println(nx, ny)
