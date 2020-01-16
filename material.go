97c3c18a685775463875a86a131a255d5874c0bf
Michael Lesniak <mail@mlesniak.com>
2020-01-16 11:54:36 +0100

    Add metal material

--- material.go
+++ material.go
@@ -14,3 +14,20 @@ func (l Lambertian) Scatter(r Ray, rec Hit) (scatter Ray, attenuation Vector, re
 	attenuation = l.Albedo
 	return scatter, attenuation, true
 }
+
+type Metal struct {
+	Albedo Vector
+}
+
+func (m Metal) Scatter(r Ray, rec Hit) (scatter Ray, attenuation Vector, reflected bool) {
+	ref := Reflect(Unit(r.Direction()), rec.Normal)
+	scatter = Ray{rec.P, ref}
+	attenuation = m.Albedo
+	return scatter, attenuation, Dot(scatter.Direction(), rec.Normal) > 0
+}
+
+func Reflect(v, n Vector) Vector {
+	d := 2.0 * Dot(v, n)
+	n2 := n.Scale(d)
+	return v.Sub(n2)
+}
